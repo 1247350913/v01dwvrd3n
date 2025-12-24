90e48659a1475507da1ec14b75f87e3752af533b
Cameron Hirsh <camjhirsh@gmail.com>
2025-12-24 16:06:23 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+document.querySelectorAll(".group-grid img").forEach(img => {
+  img.addEventListener("click", () => {
+    lightboxImg.src = img.src;
+    lightbox.classList.remove("hidden");
+  });
+});
+
+lightbox.addEventListener("click", () => {
+  lightbox.classList.add("hidden");
+  lightboxImg.src = "";
+});
